12462aafd6f4958b1658198f0166662aef2b756e
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-01-05 14:20:39 -0300

    feat(proxy config): added proxy config file to avoid CORS conflict between API and Frontend

--- angular/src/app/courses/services/courses.service.ts
+++ angular/src/app/courses/services/courses.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Course } from 'src/app/models/course';
 import { HttpClient } from '@angular/common/http';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
