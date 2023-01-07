138c22077bd83a618f00f9259fdf38aaf8003212
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-01-07 15:50:59 -0300

    feat(forms fields): added form builder with fields styles

--- angular/src/app/courses/course-form/course-form.component.ts
+++ angular/src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
+  }
+
+  onSubmit() {
+    console.log('click submit button test');
+  }
+
+  onCancel() {
+    console.log('click cancel button test');
+  }
+
+  ngOnInit(): void {}
 }
