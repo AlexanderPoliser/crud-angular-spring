3153491a7a79515659454c4fda66ae1ac6671214
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-01-07 18:32:38 -0300

    feat(course controller): added post method to create a new course

--- spring/src/main/java/com/poliser/crudspring/controllers/CourseController.java
+++ spring/src/main/java/com/poliser/crudspring/controllers/CourseController.java
@@ -2,8 +2,14 @@ package com.poliser.crudspring.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poliser.crudspring.models.Course;
@@ -22,5 +28,11 @@ public class CourseController {
     @GetMapping
     public List<Course> listCourses() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course createCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
